e1316c3930573b2196f644558be3bed988c48d02
msernio <s4095318@student.rmit.edu.au>
2026-06-18 14:59:16 +1000

    1

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
+const cards = document.querySelectorAll(".card");
+const expandedCard = document.querySelector("#expanded-card");
+const expandedTitle = document.querySelector("#expanded-title");
+const expandedDirector = document.querySelector("#expanded-director");
+const expandedSynopsis = document.querySelector("#expanded-synopsis");
+const closeBtn = document.querySelector("#close-btn");
 let timer;
 
-card.addEventListener("mouseenter", function() {
-    timer = setTimeout(function() {
-        // trigger fullscreen
-    }, 1500);
-});
+cards.forEach(function(card) {
+    card.addEventListener("mouseenter", function() {
+        timer = setTimeout(function() {
+        expandedTitle.textContent = card.dataset.title;
+        expandedDirector.textContent = card.dataset.director;
+        expandedSynopsis.textContent = card.dataset.synopsis;
+        expandedCard.style.display = "block";
+        }, 1500);
+    });
 
-card.addEventListener("mouseleave", function() {
-    clearTimeout(timer);
+    card.addEventListener("mouseleave", function() {
+        clearTimeout(timer);
+    });
 });
